5a34d817e239c499391b3b5be7116f4336e65ab8
Thiago Da silva Gpmes <103701494+Thiagodsilvagomes@users.noreply.github.com>
2022-12-26 21:40:43 -0300

    Parametros ATT
    Att de Parâmetros

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
+//Ativar Links Menu
 const links = document.querySelectorAll(".header-menu li a");
 
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
-  
+
   if (url.includes(href)) {
-    link.classList.add("ativo")
+    link.classList.add("ativo");
   }
 }
 
 links.forEach(ativarLink);
+
+//Ativar Itens do Seguro/Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
